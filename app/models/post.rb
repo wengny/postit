1a2255e527eeb21c48656619c3613cf55fd765c6
Huawei Weng <wengny@gmail.com>
2014-08-08 16:10:25 -0700

    adding better slug

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,7 +9,7 @@ class Post < ActiveRecord::Base
   validates :url, presence: true, uniqueness: true
   validates :description, presence: true
 
-  before_save :generate_slug
+  before_save :generate_slug!
 
   def total_votes
     self.up_votes - self.down_votes
@@ -26,8 +26,33 @@ class Post < ActiveRecord::Base
   def to_param
     self.slug
   end
+  
+  # check if my-google, my-google-2,my-google-3 ... exit, and then add suffix
+  def generate_slug!
+    the_slug = to_slug(self.title)
+    post = Post.find_by slug: the_slug
+    count = 2
+    while post && post != self
+      the_slug = append_suffix(the_slug, count)
+      post = Post.find_by slug: the_slug
+      count += 1
+    end
+    self.slug = the_slug
+    
+  end
 
-  def generate_slug
-    self.slug = self.title.gsub(/[^a-zA-Z\d]/i, '-').downcase
+  def append_suffix(str, count)
+    if str.split('-').last.to_i != 0  # my-google-2, my-google-3 ....
+      return str.split('-').slice(0...-1).join('-')+ "-"+count.to_s # input: "my-google-2", output: "my-google"+"-"+count
+    else # my-google
+      return str + "-" + count.to_s
+    end
+  end
+
+  def to_slug(str)
+    str.strip!                     #strip the beginnin and ending space, ! mutuate the str
+    str.gsub! /[^a-zA-Z\d]/i, '-'  #replace all non-alp and non-num character to -
+    str.gsub! /-+/, '-'            #replace multiple - to one -
+    str.downcase                           
   end
 end
