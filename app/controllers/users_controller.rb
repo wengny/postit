586b884f2e98a2f0997b9c751d1ecafacdb55fd7
Huawei Weng <wengny@gmail.com>
2014-08-12 14:22:06 -0700

    Adding time zone

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,7 +38,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :password, :password_confirmation)
+    params.require(:user).permit(:username, :password, :password_confirmation, :time_zone)
   end
 
   def set_user
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,12 @@ module ApplicationHelper
     str.starts_with?("http://") ? str : "http://#{str}"
   end
 
+  # dt = comment.created_at
   def display_datetime(dt)
+    if logged_in? && !current_user.time_zone.blank?
+      dt = dt.in_time_zone(current_user.time_zone)
+    end
+
     dt.strftime "%m/%d/%Y %l:%M%P %Z"  # 03/14/2013 9:00pm
   end
 end
