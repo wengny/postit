e79c2df1faba99f58ba641cf62ebff05e703a27e
Huawei Weng <wengny@gmail.com>
2014-08-13 11:13:49 -0700

    Fixing errors when left blank to comment body

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,10 +2,11 @@ class CommentsController <ApplicationController
   before_action :require_user
 
   def create
+    
     @post = Post.find_by slug: params[:post_id]
     @comment = @post.comments.build(params.require(:comment).permit(:body))
     @comment.creator = current_user
-    
+
     if @comment.save
       flash[:notice]= "Your comment was added."
       redirect_to post_path(@post)
